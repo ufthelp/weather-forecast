022972c260343e5e6db359df7642d06ee2df387b
msingh <msingh@xmatters.com>
2018-07-04 12:18:08 -0700

    weather-app update weather api key

--- src/app/constants/app-settings.ts
+++ src/app/constants/app-settings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static API_ENDPOINT='http://api.weatherbit.io/v2.0/current?key=60f5d492671c4a9a8f14b3478ddb0c79';
+    public static API_ENDPOINT='http://api.weatherbit.io/v2.0/current?key=a54db542af214f97a6168201a4cec91e'; // 1000 calls a day for this api
     public static WEATHER_ICON='https://images.pexels.com/photos/125457/pexels-photo-125457.jpeg?cs=srgb&dl=blue-sky-bright-clouds-125457.jpg&fm=jpg';
     public static ERROR_MSG="Invalid City name";
     public static DEFAULT_CITY="Toronto";
